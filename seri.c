81abe0d622aef84a60364776c76f919c443f205f
refaldoharianja <refaldohrj@gmail.com>
2025-11-19 12:53:53 +0700

    Create seri.c

---
+++ seri.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void resistorSeri() {
+    int n, i;
+    double r_val, total = 0;
+
+    printf("\n--- Kalkulator Resistor Seri ---\n");
+    printf("Masukkan jumlah resistor: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("Masukkan nilai Resistor %d (Ohm): ", i);
+        scanf("%lf", &r_val);
+        total += r_val;
+    }
+    printf("Total Hambatan Seri: %.2f Ohm\n", total);
+}
