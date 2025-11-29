b8ec0be898f4cdee65b0e8a1ecfbea2846017556
ariqqomara02 <ariqqomara02@gmail.com>
2025-11-29 22:00:28 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,23 @@
 #include "daya.h"
 #include "seri.h"
 #include "paralel.h"
+#include "desimal.h"
 
 int main() {
     int pilihan;
 
     do {
-        printf("=== MENU UTAMA ===\n");
+        printf("=== MENU PERHITUNGAN RANGKAIAN LISTRIK DASAR ===\n");
         printf("1. Kalkulator Hukum Ohm\n");
         printf("2. Kalkulator Daya Listrik\n");
         printf("3. Kalkulator Resistor Seri\n");
-        printf("4, Kalkulator Resistor Paralel\n");
-        printf("5. Keluar\n");
-        printf("Pilih menu (1-5): ");
+        printf("4. Kalkulator Resistor Paralel\n");
+
+        printf("=== MENU KONVERSI BILANGAN ===\n");
+        printf("5. Konversi Desimal ke Biner/Oktal/Heksadesimal\n");
+        
+        printf("6. Keluar\n");
+        printf("Pilih menu (1-6): ");
         scanf("%d", &pilihan);
 
         if (pilihan == 1) {
@@ -30,15 +35,18 @@ int main() {
             paralelCalculator();
         }
         else if (pilihan == 5) {
+            desimalConverter();
+        }
+        else if (pilihan == 6) {
             printf("Program selesai.\n");
         }
         else {
-            printf("Pilihan tidak valid.\n");
+            printf("Pilihan tidak tersedia.\n");
         }
 
         printf("\n");
 
-    } while (pilihan != 5);
+    } while (pilihan != 6);
 
     return 0;
 }
