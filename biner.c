c39d6eded06825dee536296968b40c80cafd0664
refaldoharianja <refaldohrj@gmail.com>
2025-12-02 02:01:05 +0700

    Create biner.c

---
+++ biner.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "desimal.h"
+
+void binerKeDesimal() {
+    long long n;
+    int desimal = 0, basis = 1, sisa;
+    
+    printf("\n--- Konversi Biner ke Desimal ---\n");
+    printf("Masukkan bilangan biner (contoh: 1011): ");
+    scanf("%lld", &n);
+
+    long long n_awal = n;
+    while (n != 0) {
+        sisa = n % 10; 
+        desimal = desimal + sisa * basis;
+        n = n / 10;
+        basis = basis * 2;
+    }
+
+    printf("Bilangan desimal dari %lld adalah: %d\n", n_awal, desimal);
+
+    return 0;
+}
