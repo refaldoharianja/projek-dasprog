9c7a72f991ab6246d3553d81ceeb8c8f2b91743c
refaldoharianja <refaldohrj@gmail.com>
2025-12-02 02:15:35 +0700

    Create oktal.c

---
+++ oktal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "oktal.h"
+
+void oktalKeDesimal() {
+    long long n;
+    int desimal = 0, basis = 1, digit_terakhir;
+
+    printf("\n--- Konversi Oktal ke Desimal ---\n");
+    printf("Masukkan bilangan oktal (contoh: 157): ");
+    scanf("%lld", &n);
+
+    long long n_awal = n;
+    while (n != 0) {
+        digit_terakhir = n % 10;
+        if (digit_terakhir >= 8) {
+            printf("Error: %lld bukan bilangan oktal yang valid.\n", n_awal);
+            return 1;
+        }
+
+        desimal = desimal + digit_terakhir * basis;
+        n = n / 10;
+        basis = basis * 8;
+    }
+    
+    printf("Bilangan desimal dari oktal %lld adalah: %d\n", n_awal, desimal);
+
+    return 0;
+}
